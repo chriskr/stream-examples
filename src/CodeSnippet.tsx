46313fb127b39562f7448dcb31f1aa3a715e78c5
Christian Krebs <christian.krebs.pi@gmail.com>
2023-02-11 01:27:29 +0100

    Load on connect

--- src/CodeSnippet.tsx
+++ src/CodeSnippet.tsx
@@ -1,29 +1,56 @@
-import React from "react";
-import { useAsync, AsyncOptions } from "react-async";
-import Print from "./Print";
+import React from 'react';
+import { connect } from 'rxbeach/react';
+import { from, map, startWith } from 'rxjs';
+import { switchAll } from 'rxjs';
+import Print from './Print';
 
-const getCodeSnippet = async (
-  { src }: AsyncOptions<string>,
-  { signal }: AbortController
-) => {
+const getCodeSnippet = async (src: string) => {
   try {
-    const res = await fetch(src, { signal });
-    const code = await res.text();
-    const result = code.match(
-      new RegExp(/(?:\/\/ example)(.*?)\n(.*?)(?:\/\/ end example)/, "s")
+    const response = await fetch(src);
+    if (response.status !== 200) {
+      return {
+        error: `Loading source failed: ${response.statusText}`,
+      };
+    }
+    const text = await response.text();
+    const result = text.match(
+      new RegExp(/(?:\/\/ example)(.*?)\n(.*?)(?:\/\/ end example)/, 's')
     );
-    const [, , snippet] = result!;
-    return snippet;
-  } catch (e) {
-    throw e;
+    if (!result) {
+      return { code: text };
+    }
+    const [, , code] = result;
+    return { code };
+  } catch (e: any) {
+    return { error: e.message };
   }
 };
 
-const CodeSnippet = ({ src }: { src: string }) => {
-  const { data, error } = useAsync(getCodeSnippet, { src });
-  if (error) return <p>{error.message}</p>;
-  if (data) return <Print str={data} />;
+const View = ({
+  code,
+  error,
+  loading,
+}: {
+  code?: string;
+  error?: string;
+  loading?: string;
+}) => {
+  if (loading) return <div>{loading}</div>;
+  if (error) return <div>{error}</div>;
+  if (code) return <Print str={code} />;
   return null;
+};
+
+const getLoadSrcStream = (src: string) =>
+  from([src]).pipe(
+    map(getCodeSnippet),
+    switchAll(),
+    startWith({ loading: 'loading..' })
+  );
+
+const CodeSnippet = ({ src }: { src: string }) => {
+  const ConnectedPrint = connect(View, getLoadSrcStream(src));
+  return <ConnectedPrint />;
 };
 
 export default CodeSnippet;
